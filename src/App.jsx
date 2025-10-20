54499350b4b103cb048a74e2bcf762ac1c3f0076
Wanjohi-maina <168757451+Wanjohi-maina@users.noreply.github.com>
2025-10-20 19:00:16 +0300

    formaitting prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Info from "/src/components/Info.jsx"
-import About from "/src/components/About.jsx"
-import Interest from "/src/components/Interest.jsx"
-import Footer from "/src/components/Footer.jsx"
-import './App.css'
+import Info from "/src/components/Info.jsx";
+import About from "/src/components/About.jsx";
+import Interest from "/src/components/Interest.jsx";
+import Footer from "/src/components/Footer.jsx";
+import "./App.css";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Interest />
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
-function Footer () {
-    return (
-        <footer className="footer-section">
-            <img className="footer-icons" src="../src/assets/x.svg" alt="Twitter icon"/>
-            <img className="footer-icons" src="../src/assets/facebook.svg" alt="Facebook icon"/>
-            <img src="../src/assets/instagram-icon.svg" alt="instagram icon"/>
-            <img src="../src/assets/github-icon.svg" alt="GitHub icon"/>
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer className="footer-section">
+      <img
+        className="footer-icons"
+        src="../src/assets/x.svg"
+        alt="Twitter icon"
+      />
+      <img
+        className="footer-icons"
+        src="../src/assets/facebook.svg"
+        alt="Facebook icon"
+      />
+      <img src="../src/assets/instagram-icon.svg" alt="instagram icon" />
+      <img src="../src/assets/github-icon.svg" alt="GitHub icon" />
+    </footer>
+  );
 }
-export default Footer
+export default Footer;
