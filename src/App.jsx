ee82cf9cc5327b8c26a0f621cb88ddeff3564ca6
Wanjohi-maina <168757451+Wanjohi-maina@users.noreply.github.com>
2025-10-20 19:14:04 +0300

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Info from "/src/components/Info.jsx";
 import About from "/src/components/About.jsx";
 import Interest from "/src/components/Interest.jsx";
 import Footer from "/src/components/Footer.jsx";
-import "./App.css";
 
 function App() {
   return (
